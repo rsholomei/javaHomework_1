fa4c771c0d0e65f526bbf59d4b4605d41d1a9160
ruslan <ruslansholomey@gmail.com>
2016-10-22 13:44:03 +0300

    homework_1

---
+++ homeWork/src/homeWork_1/Validator.java
@@ -0,0 +1,12 @@
+package homeWork_1;
+
+/**
+ * Created by ruslan on 22.10.16.
+ */
+public class Validator {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello");
+    }
+}
+
