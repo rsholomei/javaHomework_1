7f22172dfdf9db8e3bbf41324a67dc2c0b3de881
ruslan <ruslansholomey@gmail.com>
2016-10-28 19:11:05 +0300

    homework_1

--- homeWork/src/homeWork_1/Validator.java
+++ homeWork/src/homeWork_1/Validator.java
@@ -1,203 +1,122 @@
 package homeWork_1;
 
-
 import java.util.Scanner;
 
-public class Validator
-{
-    private Scanner scanner = new Scanner(System.in);
-    private static String numberString;
-    static boolean cond;
-    private static long numberLong;
-    private int counter;
-    private String [] prefixOper;
-    private boolean correctNumber()
+public class Validator {
+    private static Scanner scanner = new Scanner(System.in);
+    private static String number;
+    private static boolean numberContainLettersFlag;
+    private static boolean numberLengthFlag;
+    private static boolean numberValidatorFlag;
+    private static int numberSum;
+    private static String [] prefixOperator = {"039", "050", "063", "066", "067", "068", "091", "092", "093",
+                                    "094", "095", "096", "097", "098", "099"};
+    private static String enterTelephoneNumber()
     {
-        prefixOper = new String[15];
-        prefixOper[0] = "039";  prefixOper[1] = "050"; prefixOper[2] = "063"; prefixOper[3] = "066"; prefixOper[4] = "067";
-        prefixOper[5] = "068";  prefixOper[6] = "091"; prefixOper[7] = "092"; prefixOper[8] = "093"; prefixOper[9] = "094";
-        prefixOper[10] = "095"; prefixOper[11] = "096"; prefixOper[12] = "097"; prefixOper[13] = "098"; prefixOper[14] = "099";
-        numberString = scanner.next();
+        System.out.println("Введіть ваш номер");
+        number = scanner.next();
+        return number;
+    }
 
-
-        cond = false;
-
-        byte [] arrayNumberByte = numberString.getBytes();
-
+    private static boolean numberContainLetters()
+    {
+        byte [] arrayNumberByte = number.getBytes();
         for (int i = 0; i < arrayNumberByte.length; i++)
         {
-            if (arrayNumberByte[i] < 42 || arrayNumberByte[i] > 58)
+            if (arrayNumberByte[i] > 42 && arrayNumberByte[i] < 58)
             {
-                System.out.println("Номер не може містити букв");
-                System.out.println("Спробуйте ще:");
-                cond = true;
+                numberContainLettersFlag = true;
                 break;
             }
         }
-
-        if (numberString.length() == 10)
-        {
-            for (int i = 0; i < prefixOper.length; i++ )
-            {
-                if (prefixOper[i].equals(numberString.substring(0, 3)))
-                {
-                    counter++;
-                }
-            }
-            if (counter == 0)
-            {
-                System.out.println("Ви ввели невірний префікс мобільного оператора");
-                System.out.println("Спробуйте ще:");
-                cond = true;
-            }
-        }
-
-        if (numberString.length() == 13 && numberString.contains("+380"))
-        {
-            for (int i = 0; i < prefixOper.length; i++ )
-            {
-                if (prefixOper[i].equals(numberString.substring(3, 6)))
-                {
-                    counter++;
-                }
-            }
-            if (counter == 0)
-            {
-                System.out.println("Ви ввели невірний префікс мобільного оператора");
-                System.out.println("Спробуйте ще:");
-                cond = true;
-            }
-        }
-
-        if (!cond)
-        {
-            if (numberString.length() != 10 && numberString.length() > 13)
-            {
-                System.out.println("Номер занадто довгий");
-                System.out.println("Спробуйте ще:");
-                cond = true;
-            }else {
-                if (numberString.length() < 10 )
-                {
-                    System.out.println("Номер занадто короткий");
-                    System.out.println("Спробуйте ще:");
-                    cond = true;
-                }else  {
-                    if (numberString.length() > 10 && numberString.length() < 13 && !numberString.contains("+380"))
-                    {
-                        System.out.println("Ви ввели невірний код караїни");
-                        System.out.println("Спробуйте ще:");
-                        cond = true;
-                    }else {
-                        numberLong = Long.parseLong(numberString);
-
-                        if (numberString.length() == 10)
-                        {
-                            numberLong = numberLong / 1000000000;
-                        }
-
-                        if (numberString.length() == 10 && numberLong != 0)
-                        {
-                            System.out.println("Номер без коду країни має починатися з 0");
-                            System.out.println("Спробуйте ще:");
-                            cond = true;
-                        }else {
-                            if (numberString.length() == 13 && !numberString.contains("+380"))
-                            {
-                                System.out.println("Ваш номер має починатися з коду країни +380");
-                                System.out.println("Спробуйте ще:");
-                                cond = true;
-                            }
-                            else {
-                                cond = false;
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        return cond;
-
+        return numberContainLettersFlag;
     }
+
+    private static boolean numberLength()
+    {
+        if (number.length() == 10 || number.length() == 13)
+        {
+            numberLengthFlag = true;
+        }
+        return numberLengthFlag;
+    }
+
+    private static boolean numberPrefixOperation()
+    {
+        if (number.length() == 13 && number.contains("+380"))
+        {
+            number = number.substring(3, 13);
+        }
+        for (int i = 0; i < prefixOperator.length; i++ )
+        {
+            if (prefixOperator[i].equals(number.substring(0, 3)))
+            {
+                numberValidatorFlag = true;
+                break;
+            }
+        }
+        return numberValidatorFlag;
+    }
+
+    private static void numberValidator()
+    {
+        while (!numberContainLetters() || !numberLength() || !numberPrefixOperation())
+        {
+            System.out.println("Ви ввели некоректний номер. Спробуйте ще:");
+            number = scanner.next();
+        }
+    }
+
+    private static int numberPhoneSum()
+    {
+        numberValidator();
+        System.out.println("Ви ввели коректний номер:");
+        byte [] numberPhoneByte = number.getBytes();
+        int [] numberPhoneInt = new int[numberPhoneByte.length];
+        for (int i = 0; i < numberPhoneByte.length; i++)
+        {
+            numberPhoneInt[i] = numberPhoneByte[i] - 48;
+            numberSum += numberPhoneInt[i];
+        }
+        System.out.println("Перша сума = " + numberSum);
+        return numberSum;
+    }
+
+    private static int numberSumResult()
+    {
+        numberPhoneSum();
+        int counter = 1;
+        while (numberSum > 9)
+        {
+            int numeric1 = numberSum / 10;
+            int numeric2 = numberSum % 10;
+            numberSum = numeric1 + numeric2;
+            counter++;
+            System.out.println(counter + " сума = " + numberSum);
+        }
+        return numberSum;
+    }
+    private static String finalResult()
+    {
+        numberSumResult();
+        switch (numberSum)
+        {
+            case 1:
+                return "One";
+            case 2:
+                return "Two";
+            case 3:
+                return "Three";
+            case 4:
+                return "Four";
+            default:
+                return String.valueOf(numberSum);
+        }
+    }
+
     public static void main(String[] args)
     {
-        System.out.println("Введіть ваш номар телефону:");
-        Validator validator = new Validator();
-        while (validator.correctNumber() == true) {}
-
-        if (cond == false)
-        {
-            System.out.println("Ви ввели коректний номер:");
-            numberLong = Long.parseLong(numberString);
-            int sum = 0;
-            int sum1 = 0;
-            int sum2;
-            int sum3;
-
-            int [] arrayNumeric = new int[numberString.length()];
-
-            for (int i = 0;  i < numberString.length(); i++)
-            {
-                arrayNumeric [i] = (int) ((numberLong % Math.pow(10, numberString.length()-(i)))
-                        /Math.pow(10, numberString.length()-(i+1)));
-                sum1 += arrayNumeric[i];
-                sum = sum1;
-            }
-            System.out.println("Перша сума = " + sum);
-
-            int numeric1 = 0;
-            int numeric2 = 0;
-            if (sum > 10)
-            {
-                numeric1 = sum / 10;
-                numeric2 = sum % 10;
-                sum2 = numeric1 + numeric2;
-                sum = sum2;
-                System.out.println("Друга сума = " + sum);
-            }
-
-            int numeric11 = 0;
-            int numeric22 = 0;
-            if (sum > 10)
-            {
-                numeric11 = sum / 10;
-                numeric22 = sum % 10;
-                sum3 = numeric11 + numeric22;
-                sum = sum3;
-                System.out.println("Третя сума = " + sum);
-            }
-            System.out.println("Кінцевий результат:");
-
-            switch (sum) {
-                case 1:
-                    System.out.println("One");
-                    break;
-                case 2:
-                    System.out.println("Two");
-                    break;
-                case 3:
-                    System.out.println("Three");
-                    break;
-                case 4:
-                    System.out.println("Four");
-                    break;
-                case 5:
-                    System.out.println("5");
-                    break;
-                case 6:
-                    System.out.println("6");
-                    break;
-                case 7:
-                    System.out.println("7");
-                    break;
-                case 8:
-                    System.out.println("8");
-                    break;
-                case 9:
-                    System.out.println("9");
-                    break;
-            }
-        }
+        enterTelephoneNumber();
+        System.out.print("Кінцевий результат = " + finalResult());
     }
 }
